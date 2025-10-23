36ed18604f31eb186289dc2fdf9265874d61e0aa
Artiom <artyombalaban@gmail.com>
2025-10-23 11:57:06 +0300

    add js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("HELLO WORLD!")
+const input = document.getElementById('textInput');
+const output = document.getElementById('output');
+
+input.addEventListener('input', function(e) {
+    const text = e.target.value;
+    
+    if (text.trim() === '') {
+        output.textContent = 'Здесь появится ваш текст';
+        output.classList.add('empty');
+    } else {
+        output.textContent = text;
+        output.classList.remove('empty');
+    }
+});
